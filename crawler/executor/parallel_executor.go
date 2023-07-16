fa48bcd56306c02f0aa60865ff525540309ff9e0
ettore-galli <ettore.galli@gmail.com>
2023-07-16 12:09:52 +0200

    Internals

--- crawler/executor/parallel_executor.go
+++ crawler/executor/parallel_executor.go
@@ -20,58 +20,63 @@ type WorkerStatus struct {
 	Status string
 }
 
+type parallelExecutorInternals struct {
+	workersWaitGroup sync.WaitGroup
+}
+
 type ParallelExecutor[P any, M any] struct {
 	Config    ParallelExecutorConfig
 	Producer  func() []P
 	Processor func(item P) (M, error)
 	Consumer  func(item M) error
 	Monitor   func(update MonitorUpdate)
+	internals parallelExecutorInternals
+}
+
+func (executor *ParallelExecutor[P, M]) monitorEventsThread(jobMap map[int]string, jobMonQ chan WorkerStatus) {
+	for {
+		event := <-jobMonQ
+		fmt.Printf("event %v\n", event)
+		jobMap[event.Worker] = event.Status
+		executor.Monitor(MonitorUpdate{Message: "", JobMap: jobMap})
+	}
+}
+
+func (executor *ParallelExecutor[P, M]) mainWorkThread(ch chan P, wid int, jobMonitoringQueue chan WorkerStatus, jobMap map[int]string) {
+	fmt.Printf("Start worker %v\n", wid)
+	jobMonitoringQueue <- WorkerStatus{Worker: wid, Status: "start"}
+	for {
+		produced, ok := <-ch
+		if !ok {
+			break
+		}
+
+		intermediate, procErr := executor.Processor(produced)
+		if procErr != nil {
+			continue
+		}
+
+		consErr := executor.Consumer(intermediate)
+		if consErr != nil {
+			continue
+		}
+
+	}
+	executor.internals.workersWaitGroup.Done()
+	jobMonitoringQueue <- WorkerStatus{Worker: wid, Status: "end"}
+	fmt.Printf("End worker %v\n", wid)
 }
 
 func (executor *ParallelExecutor[P, M]) Perform() {
 	processingQueue := make(chan P, executor.Config.Buffer)
-	var workersWaitGroup sync.WaitGroup
+
 	var jobMonitorMap = make(map[int]string)
 	jobMonitoringQueue := make(chan WorkerStatus, executor.Config.Workers)
 
-	monitorEventsThread := func(jobMap map[int]string, jobMonQ chan WorkerStatus) {
-		for {
-			event := <-jobMonQ
-			fmt.Printf("event %v\n", event)
-			jobMap[event.Worker] = event.Status
-			executor.Monitor(MonitorUpdate{Message: "", JobMap: jobMap})
-		}
-	}
-
-	workThread := func(ch chan P, w *sync.WaitGroup, wid int, jobMap map[int]string) {
-		fmt.Printf("Start worker %v\n", wid)
-		jobMonitoringQueue <- WorkerStatus{Worker: wid, Status: "start"}
-		for {
-			produced, ok := <-ch
-			if !ok {
-				break
-			}
-
-			intermediate, procErr := executor.Processor(produced)
-			if procErr != nil {
-				continue
-			}
-
-			consErr := executor.Consumer(intermediate)
-			if consErr != nil {
-				continue
-			}
-
-		}
-		w.Done()
-		jobMonitoringQueue <- WorkerStatus{Worker: wid, Status: "end"}
-		fmt.Printf("End worker %v\n", wid)
-	}
-
-	startWorkers := func(workers int, w *sync.WaitGroup, jobMap map[int]string) {
+	startWorkers := func(workers int, jobMap map[int]string) {
 		workersRange := make([]int, workers)
 		for wid := range workersRange {
-			go workThread(processingQueue, &workersWaitGroup, wid, jobMap)
+			go executor.mainWorkThread(processingQueue, wid, jobMonitoringQueue, jobMap)
 		}
 	}
 
@@ -83,15 +88,15 @@ func (executor *ParallelExecutor[P, M]) Perform() {
 	}
 
 	executorMain := func() {
-		go monitorEventsThread(jobMonitorMap, jobMonitoringQueue)
+		go executor.monitorEventsThread(jobMonitorMap, jobMonitoringQueue)
 
-		workersWaitGroup.Add(executor.Config.Workers)
+		executor.internals.workersWaitGroup.Add(executor.Config.Workers)
 
-		go startWorkers(executor.Config.Workers, &workersWaitGroup, jobMonitorMap)
+		go startWorkers(executor.Config.Workers, jobMonitorMap)
 
 		go produceData(processingQueue)
 
-		workersWaitGroup.Wait()
+		executor.internals.workersWaitGroup.Wait()
 	}
 
 	executorMain()
