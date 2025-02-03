847f9e16374b0a8261418b36f3e023c17217b081
ettore-galli <ettore.galli@gmail.com>
2025-02-03 08:03:26 +0100

    mock

--- gobytest/hello/hello.go
+++ gobytest/hello/hello.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 
 	"example.com/hello/internal/calc"
 	"example.com/hello/internal/greeter"
@@ -12,8 +16,26 @@ func Hello() string {
 	return banner.Inline(greeter.MakeHelloGreeting())
 }
 
-func main() {
+func Greet(b io.Writer, name string) {
+	fmt.Fprintf(b, "Hello, %s!", name)
+}
+
+func printDemo() {
 	fmt.Println(Hello())
 	shape := calc.Triangle{Base: 7.0, Height: 8.0}
 	fmt.Println(shape, shape.Area())
+	Greet(os.Stdout, "Ettore")
+}
+
+func webDemo() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreetHandler)))
+}
+
+func MyGreetHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "World")
+}
+
+func main() {
+	webDemo()
+	printDemo()
 }
--- gobytest/hello/hello_test.go
+++ gobytest/hello/hello_test.go
@@ -1,11 +1,24 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestHello(t *testing.T) {
 	got := Hello()
 	want := " ___       _  _       ___    ___       _  _       ___    ___       _  _       ___                      _     _  ___\n|__ \\ ___ | || | ___ |__ \\  |__ \\ ___ | || | ___ |__ \\  |__ \\ ___ | || | ___ |__ \\  __ __ __ ___  _ _ | | __| ||__ \\\n  /_// -_)| || |/ _ \\  /_/    /_// -_)| || |/ _ \\  /_/    /_// -_)| || |/ _ \\  /_/  \\ V  V // _ \\| '_|| |/ _` |  /_/\n (_) \\___||_||_|\\___/ (_)    (_) \\___||_||_|\\___/ (_)    (_) \\___||_||_|\\___/ (_)    \\_/\\_/ \\___/|_|  |_|\\__,_| (_)"
 
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Ettore")
+	want := "Hello, Ettore!"
+	got := buffer.String()
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
