f0a66d72454de9eafc22af264d2970bbcf49cbb0
ettore-galli <ettore.galli@gmail.com>
2025-02-06 08:14:57 +0100

    basic delay

--- gobytest/hello/hello.go
+++ gobytest/hello/hello.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"example.com/hello/internal/calc"
 	"example.com/hello/internal/greeter"
@@ -35,7 +36,50 @@ func MyGreetHandler(w http.ResponseWriter, r *http.Request) {
 	Greet(w, "World")
 }
 
+type Delayer interface {
+	Sleep(d time.Duration)
+}
+
+type BasicDelayer struct {
+}
+
+func (BasicDelayer) Sleep(d time.Duration) {
+	time.Sleep(d)
+}
+
+type ConfigurableDelayer struct {
+	timeDelayer  Delayer
+	delaySeconds int64
+}
+
+func (ConfigurableDelayer) delayDuration(seconds int64) time.Duration {
+	return time.Duration(seconds) * time.Second
+}
+
+func (delayer ConfigurableDelayer) Sleep(d time.Duration) {
+	delayer.timeDelayer.Sleep(delayer.delayDuration(delayer.delaySeconds))
+}
+
+func Countdown(b io.Writer, delayer Delayer) {
+	const finalWord = "Go!"
+	const countdownStart = 3
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintf(b, "%d\n", i)
+		delayer.Sleep(1 * time.Second)
+	}
+	fmt.Fprint(b, finalWord)
+}
+
 func main() {
-	webDemo()
-	printDemo()
+	if false {
+		webDemo()
+	}
+
+	if false {
+		printDemo()
+	}
+	if true {
+		Countdown(os.Stdout, &BasicDelayer{})
+	}
+
 }
--- gobytest/hello/hello_test.go
+++ gobytest/hello/hello_test.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
+	"time"
 )
 
 func TestHello(t *testing.T) {
@@ -21,5 +23,52 @@ func TestGreet(t *testing.T) {
 	got := buffer.String()
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+type SpyDelayer struct {
+	calls int
+}
+
+func (delayer *SpyDelayer) Sleep(d time.Duration) {
+	delayer.calls++
+}
+
+func TestCountdown(t *testing.T) {
+	buffer := bytes.Buffer{}
+	spyDelayer := SpyDelayer{}
+	Countdown(&buffer, &spyDelayer)
+	want := "3\n2\n1\nGo!"
+	got := buffer.String()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+	wantCalls := 3
+	spyCalls := spyDelayer.calls
+	if spyDelayer.calls != wantCalls {
+		t.Errorf("Inconsistent number of calls: got %q, want %q", spyCalls, wantCalls)
+	}
+}
+
+type SpyInspector struct {
+	operations []string
+}
+
+func (delayer *SpyInspector) Sleep(d time.Duration) {
+	delayer.operations = append(delayer.operations, "sleep")
+}
+
+func (delayer *SpyInspector) Write(p []byte) (n int, err error) {
+	delayer.operations = append(delayer.operations, "print")
+	return 0, nil
+}
+
+func TestCountdownLogic(t *testing.T) {
+	spy := SpyInspector{}
+	Countdown(&spy, &spy)
+	wantCalls := []string{"print", "sleep", "print", "sleep", "print", "sleep", "print"}
+	spyCalls := spy.operations
+	if !reflect.DeepEqual(spyCalls, wantCalls) {
+		t.Errorf("got %q, want %q", spyCalls, wantCalls)
 	}
 }
